90444b1b86cacdfa60235e878e1cec97ce2f101c
PyPavel <grabovsky.pavel@gmail.com>
2016-04-12 13:07:53 -0700

    Added files via upload
    Unit test updated, minor changes in urgent list

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
 import sys
 from main_window_design import Ui_MainWindow
 
@@ -13,13 +13,24 @@ class ToDoList(QtWidgets.QMainWindow):
 
         #connect signals to slots
         self.ui.urgent.itemClicked.connect(self.UrgentListEdit)
-    
+
     #list edits
     def UrgentListEdit(self, item):
         if( item.text() == "Add task" ):
             item = "New task " + str(self.ui.urgent.count())
             self.ui.urgent.addItem(item) 
+        else:
+            item.setFlags(QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable)
+            item.setSelected(True)
 
+    def keyPressEvent(self, event):
+        if( event.key() == QtCore.Qt.Key_Delete ):
+            for item in self.ui.urgent.selectedItems():
+                self.ui.urgent.takeItem(self.ui.urgent.row(item))
+
+    def clearList(self):
+        pass
+        
 def main():
     app = QtWidgets.QApplication(sys.argv)
     myapp = ToDoList()
--- unit_tests.py
+++ unit_tests.py
@@ -10,18 +10,37 @@ class ToDoListTest(unittest.TestCase):
     def setUp(self):
         self.form = ToDoList()
 
-    def testAddEmptyItemToListOnClick(self):
+    def testUrgentList(self):
+        #total 1 item in list at beginning
         self.assertEqual(self.form.ui.urgent.count(), 1)
 
+        #add item on clic Add Task
         self.form.UrgentListEdit(self.form.ui.urgent.item(0))
         self.assertEqual(self.form.ui.urgent.count(), 2)
 
+        #enabled to edit on click to task
         self.form.UrgentListEdit(self.form.ui.urgent.item(1))
+        self.assertEqual(self.form.ui.urgent.item(1).flags(), QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable)
+        self.assertEqual(self.form.ui.urgent.item(1).isSelected(),True)
         self.assertEqual(self.form.ui.urgent.count(), 2)
 
-    def testEditItem(self):
-        QtTest.QTest.keyClicks(self.form.ui.urgent.item(1), "Edit task 1")
-        self.assertEqual(self.form.ui.urgent.item(1).text(), "Edit task 1")
+        #delete item on Delete press
+        QtTest.QTest.keyClick(self.form, QtCore.Qt.Key_Delete )
+        self.assertEqual(self.form.ui.urgent.count(), 1)
+
+        #clear list on clear button click
+        self.form.UrgentListEdit(self.form.ui.urgent.item(0))
+        self.form.UrgentListEdit(self.form.ui.urgent.item(0))
+        self.form.UrgentListEdit(self.form.ui.urgent.item(0))
+        self.form.UrgentListEdit(self.form.ui.urgent.item(0))
+        self.form.UrgentListEdit(self.form.ui.urgent.item(0))
+        self.form.clearList()
+        self.assertEqual(self.form.ui.urgent.count(), 1)
+
+        #can`t remove add task item
+        self.form.UrgentListEdit(self.form.ui.urgent.item(0))
+        QtTest.QTest.keyClick(self.form, QtCore.Qt.Key_Delete )
+        self.assertEqual(self.form.ui.urgent.count(), 1)
 
 if __name__ == '__main__':
     unittest.main()
